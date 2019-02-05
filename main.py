48e8e663aed8bcdc03455a0585e950fab495b3ee
Erik Taubeneck <eriktaubeneck@fb.com>
2019-02-05 09:53:24 -0500

    add lane capabilities, build timedelta from str

--- main.py
+++ main.py
@@ -1,50 +1,114 @@
-from datetime import timedelta
-
 from swimsets import SwimSet
 
 
 warmup = SwimSet(
     msg='Warmup',
-    time=timedelta(minutes=10),
+    time='10:00',
     subsets=[
-        SwimSet(200, msg='Free'),
-        SwimSet(100, msg='Kick'),
-        SwimSet(200, msg='Pull'),
+        SwimSet(250, msg='Free'),
         SwimSet(100, msg='IM'),
-    ]
+        SwimSet(150, msg='Pull'),
+        SwimSet(100, msg='Kick'),
+    ],
+    print_full_stats=True,
 )
 
-# LANE 1
-preset1 = SwimSet(
+
+preset = SwimSet(
     msg='Preset',
     subsets=[
-        SwimSet(50, 'Free, DPS', time=timedelta(seconds=50), rounds=4),
-        SwimSet(100, 'IM', time=timedelta(minutes=1, seconds=40), rounds=1),
-        SwimSet(50, 'Free, Descend', time=timedelta(seconds=55), rounds=3),
-        SwimSet(100, 'IM', time=timedelta(minutes=1, seconds=45), rounds=2),
+        SwimSet(
+            distance=50,
+            msg='Choice, Drill/Swim',
+            time_by_lanes=['0:55', '1:00', '1:05', '1:10'],
+            rounds=6
+        ),
+        SwimSet(
+            distance=25,
+            msg='Stroke, Build',
+            time_by_lanes=[':30', ':35', ':35', ':40'],
+            rounds=6
+        ),
+        SwimSet(
+            distance=50,
+            msg='Kick',
+            time_by_lanes=['1:00', '1:05', '1:15', '1:20'],
+            rounds=4
+        ),
+        SwimSet(
+            distance=50,
+            msg='Free, Desc',
+            time_by_lanes=[':50', '0:55', '1:00', '1:05'],
+            rounds=3
+        ),
+        SwimSet(
+            distance_by_lanes=[200, 200, 200, 100],
+            msg='IM',
+            time_by_lanes=['3:10', '3:35', '4:00', '2:10'],
+            rounds=1
+        ),
     ],
     print_full_stats=True,
 )
 
-mainset1 = SwimSet(
+mainset = SwimSet(
     msg='Main Set',
-    rounds=3,
+    rounds=1,
     subsets=[
-        SwimSet(100, 'Kick (Fins Optional)', time=timedelta(minutes=1, seconds=50)),
-        SwimSet(75, 'Free, Desc', time=timedelta(minutes=1, seconds=5), rounds=4),
-        SwimSet(0, 'Rest', time=timedelta(seconds=60)),
-        SwimSet(25, 'DPS', time=timedelta(seconds=30)),
-        SwimSet(25, 'Build to High Rev', time=timedelta(seconds=30)),
-        SwimSet(25, 'Build to Sprint', time=timedelta(seconds=30)),
-        SwimSet(25, 'Sprint', time=timedelta(seconds=30)),
-        SwimSet(100, 'Smooth', time=timedelta(minutes=1, seconds=45)),
+        SwimSet(
+            distance_by_lanes=[250, 250, 250, 150],
+            msg='Free',
+            time_by_lanes=['3:30', '3:55', '4:20', '2:45'],
+        ),
+
+        SwimSet(
+            distance_by_lanes=[350, 350, 350, 250],
+            msg='Free',
+            time_by_lanes=['4:50', '5:25', '6:00', '4:35'],
+        ),
+
+        SwimSet(
+            distance_by_lanes=[450, 450, 450, 350],
+            msg='Free',
+            time_by_lanes=['6:10', '6:55', '7:40', '5:25'],
+        ),
+
+        SwimSet(
+            distance=50,
+            msg='EZ',
+            time='1:00',
+        ),
+
+        SwimSet(
+            distance_by_lanes=[200, 200, 200, 0],
+            msg='IM',
+            time_by_lanes=['3:10', '3:35', '4:00', '0:00'],
+            rounds_by_lanes=[3, 2, 1, 0]
+        ),
+
     ],
     print_full_stats=True,
 )
+
+# mainset1 = SwimSet(
+#     msg='Main Set',
+#     rounds=3,
+#     subsets=[
+#         SwimSet(100, 'Kick (Fins Optional)', time=timedelta(minutes=1, seconds=50)),
+#         SwimSet(75, 'Free, Desc', time=timedelta(minutes=1, seconds=5), rounds=4),
+#         SwimSet(0, 'Rest', time=timedelta(seconds=60)),
+#         SwimSet(25, 'DPS', time=timedelta(seconds=30)),
+#         SwimSet(25, 'Build to High Rev', time=timedelta(seconds=30)),
+#         SwimSet(25, 'Build to Sprint', time=timedelta(seconds=30)),
+#         SwimSet(25, 'Sprint', time=timedelta(seconds=30)),
+#         SwimSet(100, 'Smooth', time=timedelta(minutes=1, seconds=45)),
+#     ],
+#     print_full_stats=True,
+# )
 
 workout1 = SwimSet(
-    msg='TNYA Lane 1 - LIU - Mon 2019-01-28',
-    subsets=[warmup, preset1, mainset1],
+    msg='TNYA Lane 1 - LIU - Mon 2019-02-04',
+    subsets=[warmup, preset,  mainset],
     print_full_stats=True,
 )
 
--- swimsets.py
+++ swimsets.py
@@ -8,17 +8,36 @@ class SwimSet:
     def __init__(
             self,
             distance=0,
+            distance_by_lanes=None,
             msg='',
-            time=zero_seconds,
+            time='0:00',
+            time_by_lanes=None,
             rounds=1,
+            rounds_by_lanes=None,
+            lanes=4,
             subsets=None,
             print_full_stats=False):
-        self.distance = distance
-        self.time = time
-        self.rounds = rounds
+        self.lanes = list(range(lanes))
+        if distance_by_lanes:
+            self.distance = distance_by_lanes
+        else:
+            self.distance = [distance for _ in self.lanes]
+        if time_by_lanes:
+            self.time = [self.init_time(t) for t in time_by_lanes]
+        else:
+            self.time = [self.init_time(time) for _ in self.lanes]
+        if rounds_by_lanes:
+            self.rounds = rounds_by_lanes
+        else:
+            self.rounds = [rounds for _ in self.lanes]
         self.msg = msg
         self.subsets = subsets or []
         self.print_full_stats = print_full_stats
+
+    @staticmethod
+    def init_time(time):
+        minutes, seconds = map(lambda t: int(t or 0), time.split(':'))
+        return timedelta(minutes=minutes, seconds=seconds)
 
     @property
     def is_superset(self):
@@ -26,32 +45,89 @@ class SwimSet:
 
     @property
     def total_time(self):
-        if self.time != zero_seconds or not self.is_superset:
-            return self.time * self.rounds
-        return self.rounds * sum((s.total_time for s in self.subsets), zero_seconds)
+        if max(self.time) != zero_seconds or not self.is_superset:
+            return [self.time[l] * self.rounds[l] for l in self.lanes]
+        return [self.rounds[l] * sum((s.total_time[l] for s in self.subsets), zero_seconds)
+                for l in self.lanes]
+
+    @staticmethod
+    def print_dt(dt):
+        hours, minutes, seconds = str(dt).split(':')
+        if hours != '0':
+            return str(dt)
+        elif minutes != '00':
+            return str(int(minutes)) + ':' + seconds
+        elif seconds != '00':
+            return ':' + seconds
+        return '--'
 
     @property
     def total_distance(self):
         if not self.is_superset:
-            return self.distance * self.rounds
-        return self.rounds * sum(s.total_distance for s in self.subsets)
+            return [self.distance[l] * self.rounds[l] for l in self.lanes]
+        return [self.rounds[l] * sum(s.total_distance[l] for s in self.subsets)
+                for l in self.lanes]
+
+    @property
+    def rounds_str(self):
+        if not max(self.rounds) > 1:
+            return ''
+        return f'{max(self.rounds)}x '
+
+    @property
+    def distance_str(self):
+        if not max(self.distance):
+            return ''
+        return f'{max(self.distance)} '
+
+    @property
+    def time_str(self):
+        if not max(self.time):
+            return ''
+        max_dist = max(self.distance)
+        dist = [f'{d}' if d != max_dist else '' for d in self.distance]
+        max_rounds = max(self.rounds)
+        rnds = [f'{r}' if r != max_rounds else '' for r in self.rounds]
+
+        dist_rnds = []
+        for d, r in zip(dist, rnds):
+            if d and r:
+                if d == '0' or r == '0':
+                    dist_rnds.append('')
+                else:
+                    dist_rnds.append(f'({r}x, {d})')
+            elif d:
+                dist_rnds.append(f'({d})')
+            elif r:
+                dist_rnds.append(f'({r}x)')
+            else:
+                dist_rnds.append('')
+
+        tm_str = [self.print_dt(t) + dr for t, dr in zip(self.time, dist_rnds)]
+
+        return f'\n    @ {"  ".join(tm_str)} '
+
+    @property
+    def full_stats_str(self):
+        if not self.print_full_stats:
+            return ''
+        total_times = '[' + ', '.join([self.print_dt(t) for t in self.total_time]) + ']'
+        if max(self.rounds) > 1:
+            per_round = [self.print_dt(self.total_time[l]/self.rounds[l]) for l in self.lanes]
+            return (
+                f'\n        (total time: {total_times}, '
+                f'\n         per round:  {per_round}, '
+                f'\n         total dist: {self.total_distance}'
+            )
+        else:
+            return (
+                f'\n        (total time: {total_times} '
+                f'\n         total dist: {self.total_distance}) '
+            )
 
     def pprint(self):
-        msg = ''
-        if self.rounds > 1:
-            msg += f'{self.rounds}x '
-        if self.distance:
-            msg += f'{self.distance} '
-        msg += f'{self.msg} '
-        if self.time:
-            msg += f'@{self.time} '
-        if self.print_full_stats:
-            if self.rounds > 1:
-                msg += f'(total time: {self.total_time}, {self.total_time/self.rounds} per round, '
-            else:
-                msg += f'(total time: {self.total_time}, '
-            msg += f'total distance: {self.total_distance})'
-        msg += '\n'
+        msg = f'{self.rounds_str}{self.distance_str}'
+        msg += f'{self.msg} {self.full_stats_str}{self.time_str}\n'
 
         submsgs = ''.join([s.pprint() for s in self.subsets]).split('\n')
         submsg = ''.join([f'    {s}\n' for s in submsgs if s])
