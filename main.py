bb81d2b85a30471b132779ebdf350b97ea491385
Erik Taubeneck <eriktaubeneck@fb.com>
2019-02-05 10:00:24 -0500

    remove commented code

--- main.py
+++ main.py
@@ -90,22 +90,6 @@ mainset = SwimSet(
     print_full_stats=True,
 )
 
-# mainset1 = SwimSet(
-#     msg='Main Set',
-#     rounds=3,
-#     subsets=[
-#         SwimSet(100, 'Kick (Fins Optional)', time=timedelta(minutes=1, seconds=50)),
-#         SwimSet(75, 'Free, Desc', time=timedelta(minutes=1, seconds=5), rounds=4),
-#         SwimSet(0, 'Rest', time=timedelta(seconds=60)),
-#         SwimSet(25, 'DPS', time=timedelta(seconds=30)),
-#         SwimSet(25, 'Build to High Rev', time=timedelta(seconds=30)),
-#         SwimSet(25, 'Build to Sprint', time=timedelta(seconds=30)),
-#         SwimSet(25, 'Sprint', time=timedelta(seconds=30)),
-#         SwimSet(100, 'Smooth', time=timedelta(minutes=1, seconds=45)),
-#     ],
-#     print_full_stats=True,
-# )
-
 workout1 = SwimSet(
     msg='TNYA Lane 1 - LIU - Mon 2019-02-04',
     subsets=[warmup, preset,  mainset],
